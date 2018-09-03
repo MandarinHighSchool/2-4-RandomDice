4c893fbc92c378b8f1e26b35709456027a442c1a
Charlotte Morrison <charlottemaemorrison@gmail.com>
2018-09-03 14:47:23 -0400

    Initial Commit

---
+++ src/Dice.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Dice {
+
+	public static void main(String[] args) {
+		//TODO: instantiate a new instance of the Random classs
+
+        int tries = 0;      
+        int sum = 0;
+        while (sum != 7) {
+            //TODO: create 2 int variables named roll1 and roll2
+        	//      set each one equl to a new random number.
+
+        	
+        	//TODO: create a new int variable named sum
+        	//	    set it equal to the sum of roll1 and roll2
+           
+        	
+        	//Uncomment the next line to print out your math.
+            //System.out.println(roll1 + " + " + roll2 + " = " + sum);
+            tries++;
+        }        
+        System.out.println("You won after " + tries + " tries!");
+
+
+	}
+
+}
